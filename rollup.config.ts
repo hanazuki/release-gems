e32f993c2c1977b3c93965df7e5e4fb6eb923c6c
Kasumi Hanazuki <kasumi@rollingapple.net>
2026-03-05 09:59:45 +0000

    license

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,8 +1,50 @@
+import * as fs from "node:fs";
+import * as path from "node:path";
+
 import commonjs from "@rollup/plugin-commonjs";
 import json from "@rollup/plugin-json";
 import nodeResolve from "@rollup/plugin-node-resolve";
+import terser from "@rollup/plugin-terser";
 import { defineConfig } from "rollup";
+import license from "rollup-plugin-license";
 import typescript from "rollup-plugin-typescript2";
+
+const banner = `\
+<% for (const dependency of dependencies) {
+%>---
+
+<%= dependency.name %> -- <%= dependency.version %>
+<% if (dependency.licenseText) {
+%>
+## License (<%= dependency.license %>)
+
+<%= dependency.licenseText %>
+<% }
+%><% if (dependency.noticeText) {
+%>
+## Notice
+
+<%= dependency.noticeText %>
+<% }
+%><% }
+%>`;
+
+const plugins = [
+  nodeResolve({ preferBuiltins: true }),
+  commonjs(),
+  json(),
+  typescript(),
+  terser(),
+  license({
+    banner,
+    thirdParty: {
+      includePrivate: false,
+      includeSelf: true,
+      multipleVersions: true,
+      output: [],
+    },
+  }),
+];
 
 export default defineConfig([
   {
@@ -12,12 +54,7 @@ export default defineConfig([
       format: "cjs",
       inlineDynamicImports: true,
     },
-    plugins: [
-      nodeResolve({ preferBuiltins: true }),
-      commonjs(),
-      json(),
-      typescript(),
-    ],
+    plugins,
   },
   {
     input: "src/publish.ts",
@@ -26,11 +63,6 @@ export default defineConfig([
       format: "cjs",
       inlineDynamicImports: true,
     },
-    plugins: [
-      nodeResolve({ preferBuiltins: true }),
-      commonjs(),
-      json(),
-      typescript(),
-    ],
+    plugins,
   },
 ]);
